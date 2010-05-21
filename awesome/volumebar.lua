4e6f01c690c1ebe5a305b510c2f9c0234755fb7d
Thomas Jost <thomas.jost@gmail.com>
2010-05-21 13:57:00 +0200

    awesome: support for Alsa in volumebar.lua

--- awesome/volumebar.lua
+++ awesome/volumebar.lua
@@ -1,77 +1,132 @@
-oss_channel = "misc.front"
-oss_vol_max = 58.4
-oss_mute_1 = "misc.headphone-mix.mute.headpho"
-oss_mute_2 = "misc.lineout-mix.mute.front"
-
--- Appelle un programme et renvoie la valeur lue
-function volume_cmd(cmd)
-	local pipe = io.popen(cmd)
-	local status = pipe:read("*all")
-	io.close(pipe)
-	local resultat = string.match(status, "currently set to ([0-9.ONF]+)")
-	return resultat
+function gethost()
+   local f = io.popen ("/bin/hostname")
+   local n = f:read("*a") or "none"
+   f:close()
+   return string.gsub(n, "\n$", "")
 end
 
--- Lit juste le volume
-function volume_getvol()
-	local cmd = "ossmix " .. oss_channel
-	local vol = volume_cmd(cmd)
-	vol = tonumber(vol)
-	if vol == nil then
-		return nil
-	else
-		return (vol * 100) / oss_vol_max
-	end
-end
+if gethost() == "odin" then
+   -- OSS 4
 
--- Lit juste la valeur du mode muet
-function volume_getmute()
-	local cmd = "ossmix "
-	local mute_1 = volume_cmd(cmd .. oss_mute_1)
-	local mute_2 = volume_cmd(cmd .. oss_mute_2)
-	return ((mute_1 == "ON") and (mute_2 == "ON"))
-end
+   oss_channel = "misc.front"
+   oss_vol_max = 58.4
+   oss_mute_1 = "misc.headphone-mix.mute.headpho"
+   oss_mute_2 = "misc.lineout-mix.mute.front"
 
--- Lit le volume et indique si le mode muet est activé
-function volume_get()
-	local vol = volume_getvol()
-	local mute = volume_getmute()
-	if vol == nil then return nil end
-	if mute then
-		vol = -vol
-	end
+   -- Appelle un programme et renvoie la valeur lue
+   function volume_cmd(cmd)
+      local pipe = io.popen(cmd)
+      local status = pipe:read("*all")
+      io.close(pipe)
+      local resultat = string.match(status, "currently set to ([0-9.ONF]+)")
+      return resultat
+   end
 
-	return vol
-end
+   -- Lit juste le volume
+   function volume_getvol()
+      local cmd = "ossmix " .. oss_channel
+      local vol = volume_cmd(cmd)
+      vol = tonumber(vol)
+      if vol == nil then
+         return nil
+      else
+         return (vol * 100) / oss_vol_max
+      end
+   end
 
+   -- Lit juste la valeur du mode muet
+   function volume_getmute()
+      local cmd = "ossmix "
+      local mute_1 = volume_cmd(cmd .. oss_mute_1)
+      local mute_2 = volume_cmd(cmd .. oss_mute_2)
+      return ((mute_1 == "ON") and (mute_2 == "ON"))
+   end
 
--- Augmentation du volume
-function volume_plus()
-	local cmd = "ossmix " .. oss_channel .. " -- +" .. (0.05 * oss_vol_max)
-	volume_cmd(cmd)
-	return volume_get()
-end
+   -- Lit le volume et indique si le mode muet est activé
+   function volume_get()
+      local vol = volume_getvol()
+      local mute = volume_getmute()
+      if vol == nil then return nil end
+      if mute then
+         vol = -vol
+      end
+      return vol
+   end
 
--- Diminution du volume
-function volume_minus()
-	local cmd = "ossmix " .. oss_channel .. " -- -" .. (0.05 * oss_vol_max)
-	volume_cmd(cmd)
-	return volume_get()
-end
+   -- Augmentation du volume
+   function volume_plus()
+      local cmd = "ossmix " .. oss_channel .. " -- +" .. (0.05 * oss_vol_max)
+      volume_cmd(cmd)
+      return volume_get()
+   end
 
--- Activation/désactivation du mode muet
-function volume_mute()
-	local mute = volume_getmute()
-	if mute then
-		mute = " OFF"
-	else
-		mute = " ON"
-	end
-	local cmd_1 = "ossmix " .. oss_mute_1 .. mute
-	local cmd_2 = "ossmix " .. oss_mute_2 .. mute
-	volume_cmd(cmd_1)
-	volume_cmd(cmd_2)
-	return volume_get()
+   -- Diminution du volume
+   function volume_minus()
+      local cmd = "ossmix " .. oss_channel .. " -- -" .. (0.05 * oss_vol_max)
+      volume_cmd(cmd)
+      return volume_get()
+   end
+
+   -- Activation/désactivation du mode muet
+   function volume_mute()
+      local mute = volume_getmute()
+      if mute then
+         mute = " OFF"
+      else
+         mute = " ON"
+      end
+      local cmd_1 = "ossmix " .. oss_mute_1 .. mute
+      local cmd_2 = "ossmix " .. oss_mute_2 .. mute
+      volume_cmd(cmd_1)
+      volume_cmd(cmd_2)
+      return volume_get()
+   end
+
+else
+   -- ALSA
+
+   actrl = "Master"
+
+   -- Appelle un programme et renvoie la valeur lue
+   function volume_cmd(cmd)
+      local pipe = io.popen(cmd)
+      local status = pipe:read("*all")
+      io.close(pipe)
+      local vol = string.match(status, "%[([0-9]+)%%%]")
+      local active = string.match(status, "%[(o[nf]+)%]")
+      if vol == nil then
+         return nil
+      elseif active == "off" then
+         return tonumber(vol)
+      else
+         return -tonumber(vol)
+      end
+   end
+
+   -- Lit le volume et indique si le mode muet est activé
+   function volume_get()
+      local cmd = "amixer sget " .. actrl
+      return volume_cmd(cmd)
+   end
+
+   -- Augmentation du volume
+   function volume_plus()
+      local cmd = "amixer sset " .. actrl .. " 1+"
+      return volume_cmd(cmd)
+   end
+
+   -- Diminution du volume
+   function volume_minus()
+      local cmd = "amixer sset " .. actrl .. " 1-"
+      return volume_cmd(cmd)
+   end
+
+   -- Activation/désactivation du mode muet
+   function volume_mute()
+      local cmd = "amixer sset " .. actrl .. " toggle"
+      return volume_cmd(cmd)
+   end
+
 end
 
 -- Mise à jour du widget
