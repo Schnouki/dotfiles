430d15935156ec64e82c048ecd66e17a572fad21
Thomas Jost <thomas.jost@gmail.com>
2010-10-15 15:12:45 +0200

    emacs: el-get: use recipes from my fork

--- emacs/init-20-el-get.el
+++ emacs/init-20-el-get.el
@@ -8,21 +8,14 @@
 (setq el-get-dir "~/.config/emacs/el-get"
       el-get-recipe-path '("~/.config/emacs/el-get/el-get/recipes")
       el-get-sources
-      '(erc-highlight-nicknames flyguess google-maps google-weather rainbow-mode undo-tree verbiste
+      '(erc-highlight-nicknames flyguess folding google-maps google-weather
+	ioccur python-mode rainbow-mode undo-tree verbiste
 	(:name el-get
 	       :url "git://github.com/Schnouki/el-get.git")
 	(:name magit
 	       :url "git://github.com/Schnouki/magit.git")
-	(:name folding
-	       :type http
-	       :url "http://git.savannah.gnu.org/cgit/emacs-tiny-tools.git/plain/lisp/other/folding.el?h=devel"
-	       :features (folding folding-isearch)
-	       :after 'folding-mode-add-find-file-hook)
 	(:name pymacs
 	       :build ("make" "sudo make install"))
-	(:name python-mode
-	       :type bzr
-	       :url "lp:python-mode")
 	))
 
 (el-get)
@@ -34,3 +27,4 @@
       (el-get-update (symbol-name
 		      (if (symbolp item) item
 			(plist-get item :name)))))))
+
