8b51c38caca591afe4c1c5f0ff7b4bb3725d5e59
Thomas Jost <thomas.jost@gmail.com>
2010-10-29 16:54:32 +0200

    emacs: use CEDET and ECB

--- emacs/init-00-custom.el
+++ emacs/init-00-custom.el
@@ -3,6 +3,7 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
+ '(ecb-options-version "2.40")
  '(font-latex-fontify-sectioning 1.05)
  '(inhibit-startup-screen t)
  '(org-agenda-files (quote ("~/org/gcal/pro.org" "~/org/gcal/perso.org" "~/org/migration_serveur.org" "~/org/todo.org")))
--- emacs/init-40-cedet.el
+++ emacs/init-40-cedet.el
@@ -2,6 +2,7 @@
 ;; CEDET 
 ;; -----------------------------------------------------------------------------
 
+(add-to-list 'load-path "/usr/share/emacs/site-lisp/cedet")
 (load-file "/usr/share/emacs/site-lisp/cedet/common/cedet.el")
 
 ;; Project management - EDE package
@@ -13,7 +14,46 @@
 
 ;; To use additional features for names completion, and displaying of
 ;; information for tags & classes, you also need to load the semantic-ia package
-;(require 'semantic-ia)
+(require 'semantic-ia)
 
 ;; Access to system include files
 (require 'semantic-gcc)
+
+;; Some key bindings
+;; - Completion
+(global-set-key (kbd "C-<tab>") 'semantic-ia-complete-symbol-menu)
+;; - Go to prototype
+(global-set-key (kbd "C-² C-p") 'semantic-analyze-proto-impl-toggle)
+;; - Jump to the tag refered at POINT (and back again)
+(global-set-key (kbd "C-² j") 'semantic-ia-fast-jump)
+(global-set-key (kbd "C-² b") 'semantic-mrub-switch-tags)
+;; - Navigate to next/prev/parent tag
+(global-set-key (kbd "C-² n") 'senator-next-tag)
+(global-set-key (kbd "C-² n") 'senator-previous-tag)
+(global-set-key (kbd "C-² u") 'senator-go-to-up-reference)
+;; - Search where a function is called
+(global-set-key (kbd "C-² s") 'semantic-symref)
+(global-set-key (kbd "C-² C-s") 'semantic-symref-symbol)
+
+;; Mode-specific key bindings
+(defun cedet-C-local-keys-hook ()
+  (local-set-key "." 'semantic-complete-self-insert)
+  (local-set-key ">" 'semantic-complete-self-insert))
+(add-hook 'c-mode-common-hook 'cedet-C-local-keys-hook)
+
+
+;; -----------------------------------------------------------------------------
+;; ECB -- Emacs Code Browser 
+;; -----------------------------------------------------------------------------
+(add-to-list 'load-path "/usr/share/emacs/site-lisp/ecb")
+(require 'ecb)
+
+(setq ecb-layout-name "left6")
+
+;; -----------------------------------------------------------------------------
+;; Projects 
+;; -----------------------------------------------------------------------------
+(ede-cpp-root-project "OpenNL"
+                :name "OpenNL project"
+                :file "~/Recherche/ALICE/OpenNL/opennl/CMakeLists.txt"
+                :include-path '("/src"))
