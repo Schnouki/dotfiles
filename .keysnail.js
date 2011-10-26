2f065e52c64fdb0098bafaedab0d91c44ae48784
Thomas Jost <schnouki@schnouki.net>
2011-10-26 16:35:33 +0200

    Add keysnail config
    Keysnail is a firefox addon which adds support of keyboard sequences like in
    Emacs. Quite empty for now, only 2 keys set: C-x d and C-x C-d to download stuff
    with DownThemAll.

---
+++ .keysnail.js
@@ -0,0 +1,59 @@
+// ========================== KeySnail Init File =========================== //
+
+// You can preserve your code in this area when generating the init file using GUI.
+// Put all your code except special key, set*key, hook, blacklist.
+// ========================================================================= //
+//{{%PRESERVE%
+// Put your codes here
+//}}%PRESERVE%
+// ========================================================================= //
+
+// ========================= Special key settings ========================== //
+
+key.quitKey              = "C-g";
+key.helpKey              = "<f1>";
+key.escapeKey            = "C-q";
+key.macroStartKey        = "<f3>";
+key.macroEndKey          = "<f4>";
+key.universalArgumentKey = "C-u";
+key.negativeArgument1Key = "C--";
+key.negativeArgument2Key = "C-M--";
+key.negativeArgument3Key = "M--";
+key.suspendKey           = "<f2>";
+
+// ================================= Hooks ================================= //
+
+hook.setHook('KeyBoardQuit', function (aEvent) {
+    if (key.currentKeySequence.length) {
+        return;
+    }
+    command.closeFindBar();
+    var marked = command.marked(aEvent);
+    if (util.isCaretEnabled()) {
+        if (marked) {
+            command.resetMark(aEvent);
+        } else {
+            if ("blur" in aEvent.target) {
+                aEvent.target.blur();
+            }
+            gBrowser.focus();
+            _content.focus();
+        }
+    } else {
+        goDoCommand("cmd_selectNone");
+    }
+    if (KeySnail.windowType === "navigator:browser" && !marked) {
+        key.generateKey(aEvent.originalTarget, KeyEvent.DOM_VK_ESCAPE, true);
+    }
+});
+
+
+// ============================= Key bindings ============================== //
+
+key.setViewKey(['C-x', 'd'], function (ev, arg) {
+    document.getElementById("dtaCtxTDTA").doCommand();
+}, 'DownThemAll oneClick');
+
+key.setViewKey(['C-x', 'C-d'], function (ev, arg) {
+    document.getElementById("dtaCtxDTA").doCommand();
+}, 'DownThemAll');
