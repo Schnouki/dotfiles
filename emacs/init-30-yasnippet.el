bbf8392c9644f9dffe5273f8fe062787483fb55d
Thomas Jost <schnouki@schnouki.net>
2012-07-19 14:22:54 +0200

    emacs: misc

--- emacs/init-30-yasnippet.el
+++ emacs/init-30-yasnippet.el
@@ -9,6 +9,7 @@
 ;; Modes that bind something to "<tab>" instead of "TAB"
 ;; -- http://blog.iany.me/2012/03/fix-tab-binding-for-yasnippet-and-auto-complete/
 (defun schnouki/yas-tab-noconflict ()
+  (interactive)
   (let ((command (key-binding [tab])))    ; remember command
     (local-unset-key [tab])               ; unset from (kbd "<tab>")
     (local-set-key (kbd "TAB") command))) ; bind to (kbd "TAB")
