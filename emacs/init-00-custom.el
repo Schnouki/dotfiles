03f030032204d9e18754c0d8114054e836ff9217
Thomas Jost <schnouki@schnouki.net>
2012-09-01 09:22:21 +0200

    emacs: update theme

--- emacs/init-00-custom.el
+++ emacs/init-00-custom.el
@@ -3,7 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("3580fb8e37ee9e0bcb60762b81260290329a97f3ca19249569d404fce422342f" default)))
+ '(custom-safe-themes (quote ("cc83fa4ffec1545d4bde6a44b1fb8431f9090874a22554920c709fa97338d0aa" "3580fb8e37ee9e0bcb60762b81260290329a97f3ca19249569d404fce422342f" default)))
  '(ecb-options-version "2.40")
  '(inhibit-startup-screen t)
  '(safe-local-variable-values (quote ((pkgbuild-update-sums-on-save) (py-indent-offset . 4) (ispell-dictionary . "francais") (ispell-dictionary . "american")))))
