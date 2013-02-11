38868e1571fbd683bf642a25c07e44f23fdcca23
Thomas Jost <schnouki@schnouki.net>
2013-02-11 19:04:11 +0100

    emacs: fix yasnippet in org-mode

--- emacs/init-30-yasnippet.el
+++ emacs/init-30-yasnippet.el
@@ -4,7 +4,7 @@
 
 (require 'yasnippet)
 
-(yas/global-mode 1)
+(yas-global-mode 1)
 
 ;; Modes that bind something to "<tab>" instead of "TAB"
 ;; -- http://blog.iany.me/2012/03/fix-tab-binding-for-yasnippet-and-auto-complete/
@@ -15,4 +15,3 @@
     (local-set-key (kbd "TAB") command))) ; bind to (kbd "TAB")
 (add-hook 'ruby-mode-hook 'schnouki/yas-tab-noconflict)
 (add-hook 'markdown-mode-hook 'schnouki/yas-tab-noconflict)
-(add-hook 'org-mode-hook 'schnouki/yas-tab-noconflict)
