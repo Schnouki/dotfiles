6b0217e9c1e609baa69ecda483e3d416dd52cf5f
Thomas Jost <schnouki@schnouki.net>
2013-02-28 19:28:49 +0100

    emacs: org cleanup

--- emacs/init-40-org.el
+++ emacs/init-40-org.el
@@ -32,7 +32,7 @@
 
       org-agenda-dim-blocked-tasks t
       org-agenda-files (mapcar (lambda (s) (concat org-directory s ".org"))
-			       '("todo" "cours" "perso" "pro"))
+			       '("todo"))
       org-agenda-custom-commands
       '(("c" . "TODO par catégories")
 	("cp" "Catégorie: perso" tags-todo "+perso")
@@ -94,58 +94,3 @@
   (org-overview))
 (global-set-key (kbd "C-! s") 'schnouki/org-sort-todo-list)
 (global-set-key (kbd "C-ç s") 'schnouki/org-sort-todo-list)
-
-;; Dynamically adjust tag position
-;; http://orgmode.org/worg/org-hacks.php#sec-13
-(setq ba/org-adjust-tags-column t)
-(setq schnouki/org-show-ellipsis-when-adjusting-tags t)
-
-(defun ba/org-adjust-tags-column-reset-tags ()
-  "In org-mode buffers it will reset tag position according to
-`org-tags-column'."
-  (when (and
-         (not (string= (buffer-name) "*Remember*"))
-         (eql major-mode 'org-mode))
-    (let ((b-m-p (buffer-modified-p)))
-      (condition-case nil
-          (save-excursion
-            (goto-char (point-min))
-            (command-execute 'outline-next-visible-heading)
-            ;; disable (message) that org-set-tags generates
-            (flet ((message (&rest ignored) nil))
-              (org-set-tags 1 t))
-            (set-buffer-modified-p b-m-p))
-        (error nil)))))
-
-(defun ba/org-adjust-tags-column-now ()
-  "Right-adjust `org-tags-column' value, then reset tag position."
-  (set (make-local-variable 'org-tags-column)
-       (let ((len (if org-ellipsis (length org-ellipsis) 3)))
-	 (if schnouki/org-show-ellipsis-when-adjusting-tags
-	     (- (- (window-width) len))
-	   (- (window-width)))))
-  (ba/org-adjust-tags-column-reset-tags))
-
-(defun ba/org-adjust-tags-column-maybe ()
-  "If `ba/org-adjust-tags-column' is set to non-nil, adjust tags."
-  (when ba/org-adjust-tags-column
-    (ba/org-adjust-tags-column-now)))
-
-(defun ba/org-adjust-tags-column-before-save ()
-  "Tags need to be left-adjusted when saving."
-  (when ba/org-adjust-tags-column
-    (setq org-tags-column 1)
-    (ba/org-adjust-tags-column-reset-tags)))
-
-(defun ba/org-adjust-tags-column-after-save ()
-  "Revert left-adjusted tag position done by before-save hook."
-  (ba/org-adjust-tags-column-maybe)
-  (set-buffer-modified-p nil))
-
-;; ; automatically align tags on right-hand side
-;; (add-hook 'window-configuration-change-hook
-;; 	  'ba/org-adjust-tags-column-maybe)
-;; (add-hook 'before-save-hook 'ba/org-adjust-tags-column-before-save)
-;; (add-hook 'after-save-hook 'ba/org-adjust-tags-column-after-save)
-;; (add-hook 'org-agenda-mode-hook '(lambda ()
-;; 				   (setq org-agenda-tags-column (- (window-width))))))
