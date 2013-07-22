e40c94b2221d8c06c0fa524d581c44f8bcf5f9a1
Thomas Jost <schnouki@schnouki.net>
2013-07-22 18:07:28 +0200

    emacs, zsh: minor updates

--- emacs/init-00-custom.el
+++ emacs/init-00-custom.el
@@ -3,7 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("3d6b08cd1b1def3cc0bc6a3909f67475e5612dba9fa98f8b842433d827af5d30" "f61972772958e166cda8aaf0eba700aad4faa0b4101cee319e894e7a747645c9" "a18dd0a954ac63a80e62c8cb1b550ffcf5d8461189c7c672555faadf2facfcf3" default)))
+ '(custom-safe-themes (quote ("3bedd09a2afee0d1f8d52892f740cd86f665ab1291fcdefb120963a0fa9b18d7" "3d6b08cd1b1def3cc0bc6a3909f67475e5612dba9fa98f8b842433d827af5d30" "f61972772958e166cda8aaf0eba700aad4faa0b4101cee319e894e7a747645c9" "a18dd0a954ac63a80e62c8cb1b550ffcf5d8461189c7c672555faadf2facfcf3" default)))
  '(ecb-options-version "2.40")
  '(inhibit-startup-screen t)
  '(safe-local-variable-values (quote ((js2-strict-trailing-comma-warning) (haml-indent-offset . 4) (encoding . utf-8) (pkgbuild-update-sums-on-save) (py-indent-offset . 4) (ispell-dictionary . "francais") (ispell-dictionary . "american")))))
