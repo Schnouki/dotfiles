77265379c7547eba1cf95b1a508d82ff0aa49e64
Thomas Jost <schnouki@schnouki.net>
2013-08-01 19:10:56 +0200

    emacs: magit tweak

--- emacs/init-40-vc.el
+++ emacs/init-40-vc.el
@@ -5,4 +5,9 @@
 (autoload 'magit-status "magit" nil t)
 (global-set-key (kbd "C-<") 'magit-status)
 (global-set-key (kbd "C-à") 'magit-status)
+
+(autoload 'magit-blame-mode "magit" nil t)
+(global-set-key (kbd "C-c C-<") 'magit-blame-mode)
+(global-set-key (kbd "C-c C-à") 'magit-blame-mode)
+
 (setq magit-process-popup-time 5)
