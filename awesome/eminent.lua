7eedd1ff5b5bc0d7dbc5a5fa58ad9129d8b37209
Thomas Jost <schnouki@schnouki.net>
2013-10-17 11:09:15 +0200

    awesome: update eminent
    From https://github.com/guotsuan/eminent, patched like in bba8c733.

--- awesome/eminent.lua
+++ awesome/eminent.lua
@@ -7,7 +7,7 @@
 ----------------------------------------------------------------
 -- To use this module add:
 --   require("eminent")
--- to the top of your rc.lua. 
+-- to the top of your rc.lua.
 --
 -- That's it. Through magical monkey-patching, all you need to
 -- do to start dynamic tagging is loading it.
@@ -31,8 +31,11 @@ local capi = {
     keygrabber = keygrabber,
 }
 
+local getscreen = capi.tag.getscreen
+
 -- Eminent: Effortless wmii-style dynamic tagging
-module("eminent")
+local eminent = {}
+
 
 -- Grab the original functions we're replacing
 local deflayout = nil
@@ -41,13 +44,15 @@ local orig = {
     viewidx = awful.tag.viewidx,
 
     taglist = awful.widget.taglist.new,
-    filter = awful.widget.taglist.filter.all,
+    --label = awful.widget.taglist.label.all,
+    label = awful.widget.taglist.filter.all,
 }
 
 -- Return tags with stuff on them, mark others hidden
 function gettags(screen)
     local tags = {}
 
+    --for k, t in ipairs(capi.screen[screen]:tags()) do
     for k, t in ipairs(awful.tag.gettags(screen)) do
         if t.selected or #t:clients() > 0 then
             awful.tag.setproperty(t, "hide", false)
@@ -66,26 +71,43 @@ awful.tag.new = function (names, screen, layout)
     return orig.new(names, screen, layout)
 end
 
--- Taglist filter functions
+-- Taglist label functions
+--awful.widget.taglist.label.all = function (t, args)
 awful.widget.taglist.filter.all = function (t, args)
     if t.selected or #t:clients() > 0 then
-        return orig.filter(t, args)
+        return orig.label(t, args)
     end
 end
 
 
 -- Update hidden status
 local function uc(c) gettags(c.screen) end
-local function ut(s, t) gettags(s.index) end
+--local function ut(t) gettags(t.screen) end
+local function ut(s,t) gettags(s.index) end
+
 
 capi.client.connect_signal("unmanage", uc)
 capi.client.connect_signal("new", function(c)
     c:connect_signal("property::screen", uc)
+    c:connect_signal("property::urgent", uc)
     c:connect_signal("tagged", uc)
     c:connect_signal("untagged", uc)
+    c:connect_signal("focus", uc)
+    c:connect_signal("unfocus", uc)
 end)
 
 for screen=1, capi.screen.count() do
-    awful.tag.attached_connect_signal(screen, "property::selected", uc)
+    awful.tag.attached_connect_signal(screen, "property::selected", ut)
+    awful.tag.attached_connect_signal(screen, "property::icon", ut)
+    awful.tag.attached_connect_signal(screen, "property::hide", ut)
+    awful.tag.attached_connect_signal(screen, "property::name", ut)
+    awful.tag.attached_connect_signal(screen, "property::activated", ut)
+    awful.tag.attached_connect_signal(screen, "property::screen", ut)
+    awful.tag.attached_connect_signal(screen, "property::index", ut)
+
+    --awful.tag.attached_connect_signal(screen, "tag::history::update",uc)
     capi.screen[screen]:connect_signal("tag::history::update", ut)
+    --capi.screen[screen]:connect_signal("tag::detach", ut)
 end
+
+return eminent
