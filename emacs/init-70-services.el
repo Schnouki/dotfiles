50ad0e9b7b2fd9e07b730f682bd33a727809e9d0
Thomas Jost <schnouki@schnouki.net>
2014-01-20 12:08:34 +0100

    emacs: update Prodigy services for Pelican

--- emacs/init-70-services.el
+++ emacs/init-70-services.el
@@ -9,17 +9,20 @@
 (let ((blogdir (expand-file-name "~/site/blog")))
   (prodigy-define-service
     :name "Pelican builder"
+    :tags '(blog)
     :cwd blogdir
     :command "pelican"
     :args `("--debug" "--autoreload" ,(concat blogdir "/content")
 	    "-o" ,(concat blogdir "/output") "-s" ,(concat blogdir "/pelicanconf.py"))
-    :tags '(blog))
+    :on-output (lambda (service output)
+		 (when (s-matches? "Done: Processed [0-9]+ articles" output)
+		   (prodigy-set-status service 'ready))))
 
   (prodigy-define-service
     :name "Pelican devserver"
+    :tags '(blog)
     :cwd (concat blogdir "/output")
     :command "python3"
-    :args '("-m" "pelican.server" "9090")
-    :tags '(blog)))
+    :args '("-m" "pelican.server" "9090")))
 
 ;;; init-70-services.el ends here
