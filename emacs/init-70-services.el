3c40351335f7ce8539be1465d9cc18ff362a3c5c
Thomas Jost <schnouki@schnouki.net>
2014-05-28 16:10:01 +0200

    emacs: enhance Pelican services

--- emacs/init-70-services.el
+++ emacs/init-70-services.el
@@ -12,17 +12,16 @@
       :tags '(blog)
       :cwd blogdir
       :command "pelican"
-      :args `("--debug" "--autoreload" ,(concat blogdir "/content")
-	      "-o" ,(concat blogdir "/output") "-s" ,(concat blogdir "/pelicanconf.py"))
-      :on-output (lambda (service output)
-		   (when (s-matches? "Done: Processed [0-9]+ articles" output)
-		     (prodigy-set-status service 'ready))))
+      :args (list "--debug" "--autoreload" (concat blogdir "/content")
+		  "-o" (concat blogdir "/output") "-s" (concat blogdir "/pelicanconf.py"))
+      :ready-message "Done: Processed [0-9]+ articles")
 
     (prodigy-define-service
       :name "Pelican devserver"
       :tags '(blog)
       :cwd (concat blogdir "/output")
       :command "python3"
-      :args '("-m" "pelican.server" "9090"))))
+      :args '("-c" "import logging; logging.basicConfig(level=logging.INFO, format='[%(levelname)s] %(message)s'); import pelican.server" "9090")
+      :ready-message "serving at port [0-9]+")))
 
 ;;; init-70-services.el ends here
