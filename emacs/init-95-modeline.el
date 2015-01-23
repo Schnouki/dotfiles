f37a39b1a5418aae2b387a67d7ab5a89a755ce1b
Thomas Jost <schnouki@schnouki.net>
2015-01-23 11:06:59 +0100

    emacs: move which-func where it can be seen

--- emacs/init-95-modeline.el
+++ emacs/init-95-modeline.el
@@ -23,6 +23,18 @@
 	    ("^~/findspire/findspire-front/" ":FS:")
 	    ("^:FS:findspire/" ":FSf:")
 	    ))
+
+    ;; Move which-func indicator to just after the file name
+    ;; https://github.com/Bruce-Connor/smart-mode-line/issues/77#issuecomment-39708760
+    (let ((which-func '(which-func-mode ("" which-func-format " ")))
+	  cell)
+      (setq-default mode-line-format (remove which-func mode-line-format))
+      (setq-default mode-line-misc-info (remove which-func mode-line-misc-info))
+      (setq cell (last mode-line-format 8))
+      (setcdr cell
+	      (cons which-func
+		    (cdr cell))))
+
     (sml/setup)
     (display-battery-mode 1)))
 
