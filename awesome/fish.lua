2cd637f9e5db0de96e828fcbdacce478c18bb759
Thomas Jost <schnouki@schnouki.net>
2015-06-23 09:58:45 +0200

    awesome/fish: languages support

--- awesome/fish.lua
+++ awesome/fish.lua
@@ -10,19 +10,23 @@ function fish:new()
    w:set_image(awful.util.getdir("config") .. "/icons/fortune-cookie.png")
    w:buttons(
       awful.util.table.join(
-         awful.button({ }, 1, function() w:fortune() end)
+         awful.button({ }, 1, function() w:fortune() end),
+         awful.button({ }, 3, function() w:fortune("fr") end)
    ))
    setmetatable(w, self)
    self.__index = self
    return w
 end
 
-function fish:fortune()
-    local fh = io.popen("fortune -n 100 -s")
-    local fortune = fh:read("*all")
-    fh:close()
-    fortune = string.sub(fortune, 1, -2)
-    naughty.notify({ text = fortune, timeout = 7 })
+function fish:fortune(lang)
+   if lang == nil then
+      lang = ""
+   end
+   local fh = io.popen("fortune -a " .. lang)
+   local fortune = fh:read("*all")
+   fh:close()
+   fortune = string.sub(fortune, 1, -2)
+   naughty.notify({ text = fortune, timeout = 7 })
 end
 
 function new()
