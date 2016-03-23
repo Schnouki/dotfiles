a29407ba63f53c7fb7942c99baae50cbbc7ecfe7
Thomas Jost <schnouki@schnouki.net>
2016-03-23 11:49:51 +0100

    emacs: 25.1 pretest

--- emacs/init-00-custom.el
+++ emacs/init-00-custom.el
@@ -7,6 +7,9 @@
  '(ecb-options-version "2.40")
  '(inhibit-startup-screen t)
  '(magit-fetch-arguments (quote ("--prune")))
+ '(package-selected-packages
+   (quote
+    (zenburn-theme yasnippet yaml-mode websocket virtualenvwrapper vala-mode use-package unicode-troll-stopper unicode-fonts undo-tree unbound twittering-mode systemd sudoku smex smart-mode-line smart-forward shrink-whitespace scss-mode restclient request rainbow-mode python-environment puppet-mode prodigy popup pkgbuild-mode php-mode org oauth2 notmuch nose mc-extras markdown-mode magit-gitflow magit-annex lua-mode json-rpc js2-mode jinja2-mode jabber ix iedit ido-vertical-mode ido-ubiquitous ido-clever-match ibuffer-vc hydra hy-mode highlight-indentation hideshowvis haskell-mode haml-mode groovy-mode graphviz-dot-mode goto-last-change google-translate google-this go-eldoc gitignore-mode git-annex geiser fuzzy fringe-helper flycheck-pos-tip fixmee evil-numbers es-mode erlang epc engine-mode emojify dtrt-indent drag-stuff dockerfile-mode deft dash-functional ctags-update company-inf-python company-go company-emoji company-anaconda color-theme coffee-mode bonjourmadame bats-mode avy-zap auctex alert adoc-mode actionscript-mode ack-and-a-half)))
  '(safe-local-variable-values
    (quote
     ((python-venv . "findspire")
--- emacs/init.el
+++ emacs/init.el
@@ -3,6 +3,13 @@
 ;; This file is loaded from ~/.emacs. It must load all init-XX-blabla.el files
 ;; in ~/.config/emacs (where XX is a number and blabla a short description).
 
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (defun schnouki/init-emacs ()
   "Load various emacs init files"
   (interactive)
