49dc40aa7e533ee899282f0fbf2fb25db66a7e58
Thomas Jost <schnouki@schnouki.net>
2017-11-14 13:25:06 +0100

    emacs/org: cleanup

--- emacs/init-40-org.el
+++ emacs/init-40-org.el
@@ -7,8 +7,7 @@
   :bind (("C-! l" . org-store-link)
 	 ("C-! a" . org-agenda)
 	 ("C-! b" . org-iswitchb)
-	 ("C-! t" . schnouki/org-agenda-and-todo-list)
-	 ("C-! s" . schnouki/org-sort-todo-list))
+	 ("C-! t" . schnouki/org-agenda-and-todo-list))
   :config
   (progn
     (bind-key "C-M-g" 'org-plot/gnuplot org-mode-map)
@@ -80,23 +79,5 @@
   ;; Then display agenda
   (org-agenda-list)
   (fit-window-to-buffer nil nil 20))
-
-;; Get the index of a todo keyword in the schnouki/org-todo-keywords-sort-order list
-;; Used to sort a todo-list by todo order
-(defun schnouki/org-sort-by-todo-keywords ()
-  (interactive)
-  (if (looking-at org-complex-heading-regexp)
-      (string-position (match-string 2) schnouki/org-todo-keywords-sort-order)))
-
-;; Sort todo-list
-(defun schnouki/org-sort-todo-list ()
-  "Sort buffer in alphabetical order, then by status, then by priority."
-  (interactive)
-  (save-excursion
-    (mark-whole-buffer)
-    (org-sort-entries nil ?a)
-    (org-sort-entries nil ?f 'schnouki/org-sort-by-todo-keywords)
-    (org-sort-entries nil ?p))
-  (org-overview))
 
 ;;; init-40-org.el ends here
