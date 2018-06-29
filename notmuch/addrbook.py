61fc2f4f4a3b6fc45d0b7dce5adc9753829e3f7a
Thomas Jost <schnouki@schnouki.net>
2018-06-29 10:13:26 +0200

    notmuch: black addrbook.py

--- notmuch/addrbook.py
+++ notmuch/addrbook.py
@@ -15,7 +15,8 @@ import notmuch
 
 _DBPATH = "~/.config/notmuch/addrbook.db"
 
-#from profilehooks import profile
+# from profilehooks import profile
+
 
 class AddrBook(object):
     """An address book"""
@@ -28,7 +29,7 @@ class AddrBook(object):
             with open(os.path.expanduser(_DBPATH), "rb") as data:
                 self._db = pickle.load(data)
 
-    #@profile
+    # @profile
     def index(self, msgs: notmuch.Messages) -> None:
         """Index messages in the address book"""
         tot_msg = 0
@@ -50,15 +51,20 @@ class AddrBook(object):
                 tot_addr += self._add(addrs)
                 tot_msg += 1
                 if (tot_msg % 20) == 0:
-                    logging.debug("Messages: %d; addresses: %d", tot_msg, tot_addr)
+                    logging.debug("Messages: %d; addresses: %d",
+                                  tot_msg, tot_addr)
         finally:
             # At the end, save the DB
             self._merge_db()
             with open(os.path.expanduser(_DBPATH), "wb") as fout:
                 pickle.dump(self._db, fout, pickle.HIGHEST_PROTOCOL)
-            logging.info("Total: indexed %d messages and %d addresses. "
-                         "%d unique addresses in the address book.",
-                         tot_msg, tot_addr, len(self._db))
+            logging.info(
+                "Total: indexed %d messages and %d addresses. "
+                "%d unique addresses in the address book.",
+                tot_msg,
+                tot_addr,
+                len(self._db),
+            )
 
     def _add(self, addrs: typing.List[typing.Tuple[str, str]]) -> int:
         cnt = 0
@@ -76,8 +82,8 @@ class AddrBook(object):
                         logging.warning(e)
                         logging.warning("Faulty data: %s", hdr)
                 else:
-                    user = str(BeautifulSoup(hdr[0], 'html.parser'))
-                    if '<html>' in user:
+                    user = str(BeautifulSoup(hdr[0], "html.parser"))
+                    if "<html>" in user:
                         logging.warning("Faulty data: %s", user)
 
             if addr not in self._small_db:
@@ -110,8 +116,10 @@ class AddrBook(object):
         # Init: construct a "flat DB" of "User <addr>" => addr. For each
         # address, only keep the 3 most frequent usernames.
         flat_db = {}
+
         def _get_user_freq(addr: str) -> typing.Callable[[str], str]:
             return lambda user: self._db[addr][user]
+
         for addr in self._db:
             users = list(self._db[addr].keys())
             users.sort(key=_get_user_freq(addr), reverse=True)
@@ -122,7 +130,7 @@ class AddrBook(object):
                 flat_db[full_addr] = addr
 
         # Step 1: find matching entries
-        #needle = (BeautifulSoup(needle)).lower()
+        # needle = (BeautifulSoup(needle)).lower()
         needle = needle.lower()
         matches = filter(lambda s: s.lower().find(needle) >= 0, flat_db.keys())
 
@@ -134,13 +142,18 @@ class AddrBook(object):
                 match_addr.append(addr)
 
         # Step 3: sort by frequency
-        _get_addr_freq = lambda addr: sum(self._db[addr].values())
+        def _get_addr_freq(addr):
+            return sum(self._db[addr].values())
+
         match_addr.sort(key=_get_addr_freq, reverse=True)
 
         # Step 4: display results
         match_addr = match_addr[:25]
         for addr in match_addr:
-            _get_user_freq = lambda user: self._db[addr][user]
+
+            def _get_user_freq(user):
+                return self._db[addr][user]
+
             users = list(self._db[addr].keys())
             users.sort(key=_get_user_freq, reverse=True)
             user = users[0]
@@ -150,6 +163,7 @@ class AddrBook(object):
             else:
                 print(addr)
 
+
 def _main():
     logging.basicConfig(level=logging.INFO,
                         format="%(levelname)s: %(message)s")
@@ -157,8 +171,10 @@ def _main():
     # If called without any argument, this is supposed to be a first run: index everything
     if len(sys.argv) == 1:
         if os.path.exists(os.path.expanduser(_DBPATH)):
-            print("%s already exists.\n"
-                  "Please delete it if you want to recreate the database." % _DBPATH)
+            print(
+                "%s already exists.\n"
+                "Please delete it if you want to recreate the database." % _DBPATH
+            )
             sys.exit(1)
 
         book = AddrBook(True)
