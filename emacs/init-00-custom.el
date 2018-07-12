4518a0bb9dd97a0162efe1420ce91e093f44755d
Thomas Jost <schnouki@schnouki.net>
2018-07-12 16:50:20 +0200

    emacs: update 00-custom

--- emacs/init-00-custom.el
+++ emacs/init-00-custom.el
@@ -10,26 +10,26 @@
  '(magit-log-section-arguments (quote ("--decorate" "-n256")))
  '(package-selected-packages
    (quote
-    (cython-mode orgtbl-aggregate multiple-cursors diminish helpful emojify-logos counsel swiper ivy auto-virtualenvwrapper pomidor less-css-mode less-mode dired-collapse dired-subtree py-autopep8 ido-completing-read+ amx move-text browse-kill-ring ibuffer-projectile auto-read-only dumb-jump dump-jump editorconfig list-utils which-key flycheck-plantuml guess-language toml-mode godoctor js-doc wgrep-ag wgrep ag origami avy-flycheck go-add-tags sass-mode flycheck-ledger go-rename go-guru web-mode plantuml-mode handlebars-mode kotlin-mode tide wttrin viking-mode volatile-highlights ggtags projectile flx-ido oracle zenburn-theme yasnippet yaml-mode websocket vala-mode use-package unicode-troll-stopper undo-tree unbound twittering-mode systemd sudoku smart-mode-line smart-forward shrink-whitespace scss-mode restclient request rainbow-mode python-environment puppet-mode prodigy popup pkgbuild-mode php-mode org oauth2 nose markdown-mode magit-gitflow magit-annex lua-mode json-rpc js2-mode jinja2-mode ix iedit ido-vertical-mode ido-clever-match hydra hy-mode highlight-indentation haskell-mode haml-mode groovy-mode graphviz-dot-mode goto-last-change google-translate google-this go-eldoc gitignore-mode git-annex geiser fuzzy fringe-helper flycheck-pos-tip evil-numbers es-mode erlang epc engine-mode emojify dtrt-indent dockerfile-mode deft dash-functional ctags-update company-go company-emoji company-anaconda color-theme coffee-mode bonjourmadame bats-mode avy-zap auctex alert adoc-mode actionscript-mode smart-comment ido-occur)))
+    (rg ripgrep magit-todos hl-todo blacken cython-mode orgtbl-aggregate multiple-cursors diminish helpful emojify-logos counsel swiper ivy auto-virtualenvwrapper pomidor less-css-mode less-mode dired-collapse dired-subtree py-autopep8 ido-completing-read+ amx move-text browse-kill-ring ibuffer-projectile auto-read-only dumb-jump dump-jump editorconfig list-utils which-key flycheck-plantuml guess-language toml-mode godoctor js-doc wgrep origami avy-flycheck go-add-tags sass-mode flycheck-ledger go-rename go-guru web-mode plantuml-mode handlebars-mode kotlin-mode tide wttrin viking-mode volatile-highlights ggtags projectile flx-ido oracle zenburn-theme yasnippet yaml-mode websocket vala-mode use-package unicode-troll-stopper undo-tree unbound twittering-mode systemd sudoku smart-mode-line smart-forward shrink-whitespace scss-mode restclient request rainbow-mode python-environment puppet-mode prodigy popup pkgbuild-mode php-mode org oauth2 nose markdown-mode magit-gitflow magit-annex lua-mode json-rpc js2-mode jinja2-mode ix iedit ido-vertical-mode ido-clever-match hydra hy-mode highlight-indentation haskell-mode haml-mode groovy-mode graphviz-dot-mode goto-last-change google-translate google-this go-eldoc gitignore-mode git-annex geiser fuzzy fringe-helper flycheck-pos-tip evil-numbers es-mode erlang epc engine-mode emojify dtrt-indent dockerfile-mode deft dash-functional ctags-update company-go company-emoji company-anaconda color-theme coffee-mode bonjourmadame bats-mode avy-zap auctex alert adoc-mode actionscript-mode smart-comment ido-occur)))
  '(safe-local-variable-values
    (quote
     ((schnouki/disable-autopep8 . t)
      (py-autopep8-options quote
-                          ("--max-line-length=100"))
+			  ("--max-line-length=100"))
      (py-autopep8-options "--max-line-length=100")
      (plantuml-output-type . "svg")
      (org-image-actual-width . 120)
      (origami-fold-style . triple-braces)
      (eval when
-           (require
-            (quote rainbow-mode)
-            nil t)
-           (rainbow-mode 1))
+	   (require
+	    (quote rainbow-mode)
+	    nil t)
+	   (rainbow-mode 1))
      (eval when
-           (require
-            (quote rainbow-mode)
-            nil t)
-           (schnouki/rainbow-mode-zenburn))
+	   (require
+	    (quote rainbow-mode)
+	    nil t)
+	   (schnouki/rainbow-mode-zenburn))
      (js2-basic-offset . 4)
      (python-venv . "findspire")
      (nxml-child-indent . 2)
