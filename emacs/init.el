5d9afe04697a9f793b3feea2eb03697baa4a2ee1
Thomas Jost <schnouki@schnouki.net>
2018-08-29 11:23:48 +0200

    emacs: use sd-notify to tell systemd about the service startup
    Including a basic pure-elisp implementation of sd_notify. (I suck at Emacs
    networking btw…)

--- emacs/init.el
+++ emacs/init.el
@@ -12,10 +12,49 @@
 ;; You may delete these explanatory comments.
 (package-initialize)
 
+(defvar schnouki/sd-notify--client nil)
+(defvar schnouki/sd-notify--queue '())
+
+(defun schnouki/sd-notify--connect ()
+  (unless schnouki/sd-notify--client
+    (let ((addr (getenv "NOTIFY_SOCKET")))
+      (cond (addr
+	     (setq schnouki/sd-notify--client
+		   (make-network-process :name "sd-notify"
+					 :type 'datagram
+					 :family 'local
+					 :service addr
+					 :sentinel 'schnouki/sd-notify--sentinel
+					 :coding 'utf-8
+					 :noquery t)))
+	    (t (message "Can't notify systemd: NOTIFY_SOCKET is not set"))))))
+
+(defun schnouki/sd-notify--maybe-dump-queue ()
+  (when (and schnouki/sd-notify--client
+	     (eq (process-status schnouki/sd-notify--client) 'open))
+    (dolist (item schnouki/sd-notify--queue)
+      (process-send-string schnouki/sd-notify--client item))
+    (setq schnouki/sd-notify--queue '())))
+
+(defun schnouki/sd-notify--sentinel (proc ev)
+  (schnouki/sd-notify--maybe-dump-queue))
+
+(defun schnouki/sd-notify (state)
+  "Notify systemd that the service state changed to STATE."
+  (schnouki/sd-notify--connect)
+  (add-to-list 'schnouki/sd-notify--queue state t)
+  (schnouki/sd-notify--maybe-dump-queue))
+
 (defun schnouki/init-emacs ()
   "Load various Emacs init files."
+  (advice-add 'server-start :after #'schnouki/init-emacs--notify-ready)
   (dolist (file (directory-files "~/.config/emacs" t "^init-[0-9]+-.+\.el$"))
+    (schnouki/sd-notify (format "STATUS=Loading %s" file))
     (load file)))
+
+(defun schnouki/init-emacs--notify-ready ()
+  (schnouki/sd-notify "STATUS=Ready\nREADY=1"))
+
 
 (schnouki/init-emacs)
 
