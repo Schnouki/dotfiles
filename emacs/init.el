dcfc75b0c0163ada4e45423afbf077a22cb750b4
Thomas Jost <schnouki@schnouki.net>
2018-08-29 09:49:25 +0200

    emacs: improve init.el

--- emacs/init.el
+++ emacs/init.el
@@ -1,8 +1,10 @@
-;; init.el
-;;
+;;; init.el -- Emacs configuration
+
+;;; Commentary:
 ;; This file is loaded from ~/.emacs. It must load all init-XX-blabla.el files
 ;; in ~/.config/emacs (where XX is a number and blabla a short description).
 
+;;; Code:
 
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
@@ -11,8 +13,10 @@
 (package-initialize)
 
 (defun schnouki/init-emacs ()
-  "Load various emacs init files"
-  (interactive)
-  (dolist (file (directory-files "~/.config/emacs" nil "^init-[0-9]+-.+\.el$"))
-    (load (concat "~/.config/emacs/" file))))
+  "Load various Emacs init files."
+  (dolist (file (directory-files "~/.config/emacs" t "^init-[0-9]+-.+\.el$"))
+    (load file)))
+
 (schnouki/init-emacs)
+
+;;; init.el ends here
