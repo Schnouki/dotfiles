b7458aa1da880376279805f592a8b47c4f518d00
Thomas Jost <schnouki@schnouki.net>
2019-01-08 10:23:28 +0100

    emacs: correctly fontify Hugo shortcodes in Markdown files.

--- emacs/init-40-blog.el
+++ emacs/init-40-blog.el
@@ -12,6 +12,22 @@
 	easy-hugo-root "/srv/http/schnouki.net/htdocs-hugo"
 	easy-hugo-image-directory "img"))
 
+(defun schnouki/markdown-maybe-add-shortcode-keyword ()
+  "Enable fontifying Hugo shortcodes if in the ~/blog/ directory."
+  (when (string= (projectile-project-root) (expand-file-name easy-hugo-basedir))
+    (let ((shortcode-regexp
+           (rx (group "{{" (or "<" "%") (1+ space))            ; opening {{< or {{%
+               (group (1+ (not space)))                        ; shortcode name
+               (group (*? any))                                ; parameters
+               (group (1+ space) (? "/") (or ">" "%") "}}")))) ; closing >}}, %}}, />}} or /%}}
+      (font-lock-add-keywords nil `((,shortcode-regexp . ((1 'markdown-markup-face)
+                                                          (2 'markdown-metadata-key-face)
+                                                          (3 'markdown-metadata-value-face)
+                                                          (4 'markdown-markup-face))))))))
+
+(add-hook 'markdown-mode-hook #'schnouki/markdown-maybe-add-shortcode-keyword)
+
+
 ;; Useless :)
 (use-package company-emoji
   :ensure t
