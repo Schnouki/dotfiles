d2c34dd5d9b18b8d6fc828f04e2cf3fcc1bb7c8b
Thomas Jost <schnouki@schnouki.net>
2019-01-08 10:20:58 +0100

    emacs: better startup notifications

--- emacs/init-15-package.el
+++ emacs/init-15-package.el
@@ -30,6 +30,18 @@
 (alert (concat "Emacs " emacs-version " is loading…")
        :severity 'low
        :icon "emacs"
-       :title "Emacs")
+       :title "Emacs"
+       :id 'emacs-startup)
+
+(defvar schnouki/init-emacs--ready-alerted nil)
+(defun schnouki/init-emacs--alert-ready (&rest args)
+  (unless schnouki/init-emacs--ready-alerted
+    (alert (concat "Emacs " emacs-version" loaded!")
+	   :severity 'low
+	   :icon "emacs"
+	   :title "Emacs"
+	   :id 'emacs-startup)
+    (setq schnouki/init-emacs--ready-alerted t)))
+(advice-add 'server-start :after #'schnouki/init-emacs--alert-ready)
 
 ;;; init-15-package.el ends here
