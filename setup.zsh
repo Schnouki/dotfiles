2abc4861d5ff58bc6d49a0a5dbc5dbeae7bea71c
Thomas Jost <schnouki@schnouki.net>
2019-05-07 10:31:17 +0200

    Update encrypted stuff

--- setup.zsh
+++ setup.zsh
@@ -3,3 +3,6 @@
 # Setup ~/.zshenv and ~/.zshrc
 setup_file_symlink "zsh/profile" ".zshenv"
 setup_file_symlink "zsh/zshrc"   ".zshrc"
+
+# Setup ~/.config/zsh/private
+setup_decrypt "zsh/private.gpg" ".config/zsh/private"
