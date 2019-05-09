1c872518a4e14e4caf34bc1eda86adc0d9772811
Thomas Jost <schnouki@schnouki.net>
2019-05-09 21:26:34 +0200

    zsh: add .zlogin & .zlogout

--- setup.zsh
+++ setup.zsh
@@ -1,8 +1,16 @@
 #!/bin/zsh
 
-# Setup ~/.zshenv and ~/.zshrc
+# Setup startup/shutdown files
+# Order:
+# - ~/.zshenv
+# - login shell: /etc/zprofile, ~/.zprofile
+# - interactive shell: /etc/zshrc, ~/.zshrc
+# - login shell: /etc/zlogin, ~/.zlogin
+# On exit via exit, logout, or end-of-file: ~/.zlogout, /etc/zlogout
 setup_file_symlink "zsh/profile" ".zshenv"
 setup_file_symlink "zsh/zshrc"   ".zshrc"
+setup_file_symlink "zsh/login"   ".zlogin"
+setup_file_symlink "zsh/logout"  ".zlogout"
 
 # Setup ~/.config/zsh/private
 setup_decrypt "zsh/private.gpg" ".config/zsh/private"
