887b61f2f7a4cbf2388b52bd27579afdddcb22b7
Thomas Jost <schnouki@schnouki.net>
2020-09-30 09:37:45 +0200

    fish: improve virtualfish configuration

--- fish/conf.d/virtualfish-loader.fish
+++ fish/conf.d/virtualfish-loader.fish
@@ -1,4 +1,8 @@
 set -g VIRTUALFISH_VERSION 2.1.0
 set -g VIRTUALFISH_PYTHON_EXEC /usr/bin/python
+set -g VIRTUALFISH_ACTIVATION_FILE .workon
+
 source /usr/lib/python3.8/site-packages/virtualfish/virtual.fish
+source /usr/lib/python3.8/site-packages/virtualfish/compat_aliases.fish
+source /usr/lib/python3.8/site-packages/virtualfish/auto_activation.fish
 emit virtualfish_did_setup_plugins
