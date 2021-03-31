3bf769d94ac3b3121d7c4b07e12e400da825f151
Thomas Jost <schnouki@schnouki.net>
2021-03-31 16:22:55 +0200

    fish: better prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,5 +1,23 @@
-function fish_prompt --description 'Write out the prompt'
+function _hydro_prompt_pwd --on-variable PWD
+    # From https://github.com/jorgebucaran/hydro/blob/main/conf.d/hydro.fish
+    set -l dir_color (set_color normal)(set_color white)
+    set -l sep_color (set_color normal)(set_color --dim white)
+    set -l hl_color (set_color normal)(set_color --bold white)
 
+    set -l root (command git rev-parse --show-toplevel 2>/dev/null |
+        string replace --all --regex -- "^.*/" "")
+    set -l _pwd (
+        string replace --ignore-case -- ~ \~ $PWD |
+        string replace -- "/$root/" /:/ |
+        string replace --regex --all -- "(\.?[^/]{1})[^/]*/" \$1/ |
+        string replace -- : "$root" |
+        string replace --regex -- '([^/]+)$' "$hl_color\$1$dir_color" |
+        string replace --regex --all -- '(?!^/$)/' "$sep_color/$dir_color"
+    )
+    set --global _hydro_pwd "$dir_color$_pwd"
+end
+
+function fish_prompt --description 'Write out the prompt'
     # Config
     set -l prefix ""
     set -l suffix " "(set_color normal)
@@ -9,13 +27,17 @@ function fish_prompt --description 'Write out the prompt'
     set -l other_user_color (set_color --bold green)
     set -l hostname_separator (set_color --bold yellow)"@"
     set -l hostname_color (set_color --bold white)
-    set -l dir_color (set_color --bold white)
     set -l venv_color (set_color normal)(set_color cyan)
 
     # Actual logic
     set -l id_part ""
     set -l prompt $prompt_user
     set -l venv ""
+
+    # Make sure we have a pwd
+    if test -z "$_hydro_pwd"
+        _hydro_prompt_pwd
+    end
 
     if test "$USER" = "root"
         # If root, use a different prompt
@@ -41,5 +63,5 @@ function fish_prompt --description 'Write out the prompt'
         set venv $venv_color"("(basename "$VIRTUAL_ENV")") "
     end
 
-    echo -n $prefix$id_part$dir_color(prompt_pwd) $venv$prompt$suffix
+    echo -n $prefix$id_part$_hydro_pwd $venv$prompt$suffix
 end
