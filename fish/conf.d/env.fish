58ba868c342ad2c2c9630327be610969e0ed6943
Thomas Jost <schnouki@schnouki.net>
2021-04-20 12:18:11 +0200

    fish: add luarocks, LUA_PATH and LUA_CPATH

--- fish/conf.d/env.fish
+++ fish/conf.d/env.fish
@@ -3,9 +3,10 @@ set PATH \
     $HOME/bin \
     $HOME/.local/bin \
     $HOME/.cabal/bin \
-    $HOME/.go/bin \
-    $HOME/.gem/ruby/2.5.0/bin \
     $HOME/.composer/vendor/bin \
+    $HOME/.gem/ruby/2.5.0/bin \
+    $HOME/.go/bin \
+    $HOME/.luarocks/bin \
     $HOME/.nimble/bin \
     $PATH
 
@@ -21,6 +22,25 @@ set -gx BROWSER firefox
 
 # Go
 set -gx GOPATH $HOME/.go
+
+# Lua & Luarocks
+set -l _lua 5.4
+set -gx LUA_PATH (string join ";" \
+    /usr/share/lua/$_lua/\?.lua \
+    /usr/share/lua/$_lua/\?/init.lua \
+    /usr/lib/lua/$_lua/\?.lua \
+    /usr/lib/lua/$_lua/\?/init.lua \
+    ./\?.lua \
+    ./\?/init.lua \
+    $HOME/.luarocks/share/lua/$_lua/\?.lua \
+    $HOME/.luarocks/share/lua/$_lua/\?/init.lua \
+)
+set -gx LUA_CPATH (string join ";" \
+    /usr/lib/lua/$_lua/\?.so \
+    /usr/lib/lua/$_lua/loadall.so \
+    ./\?.so \
+    $HOME/.luarocks/lib/lua/$_lua/\?.so \
+)
 
 # Python
 set -gx PYTHONSTARTUP $HOME/.config/python_startup.py
