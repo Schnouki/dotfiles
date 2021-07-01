b081298134be8bfe0e4798e987ab45dc83014d30
Thomas Jost <schnouki@schnouki.net>
2021-07-01 14:59:40 +0200

    emacs: update my whole note-taking setup

--- emacs/init-40-org.el
+++ emacs/init-40-org.el
@@ -13,15 +13,22 @@
 	 ("C-c C-x i" . org-id-get-create)
 	 ("C-c y" . schnouki/org-yank-link))
   :custom
+  (org-adapt-indentation nil)
   (org-checkbox-hierarchical-statistics nil)
-  (org-directory "~/Dropbox/org/")
-  (org-id-link-to-org-use-id 'create-if-interactive-and-no-custom-id)
-  (org-support-shift-select t)
   (org-cycle-separator-lines 3)
+  (org-directory "~/Dropbox/Notes/")
+  (diary-file "~/Dropbox/Notes/diary")
+  (org-id-link-to-org-use-id 'create-if-interactive-and-no-custom-id)
   (org-startup-with-inline-images t)
+  (org-support-shift-select t)
   (calendar-date-style 'european)
   (calendar-week-start-day 1)
-  (diary-file (concat org-directory "diary"))
+
+  :init
+  (defun schnouki/org-mode-hook ()
+    (electric-indent-local-mode -1))
+  ;;(add-hook 'org-mode-hook #'schnouki/org-mode-hook)
+  ;;(remove-hook 'org-mode-hook #'schnouki/org-mode-hook)
 
   :config
   ;; Babel
@@ -48,21 +55,23 @@
       (org-insert-link nil location desc)))
   )
 
-
 (use-package ox-md
   :after org)
 (use-package ox-gfm
   :ensure t
   :after org)
 
-(use-package org-brain
+(use-package org-contrib
   :ensure t
-  :after org
-  :bind (:map schnouki-prefix-map
-	      ("; b" . org-brain-visualize)
-	      ("; r" . org-brain-refile))
-  :custom
-  (org-brain-backlink "← "))
+  :after org)
 
+(use-package company-org-block
+  :ensure t
+  :after (org company)
+  :custom
+  (company-org-block-edit-style 'auto)
+  :hook (org-mode . (lambda ()
+		       (add-to-list (make-local-variable 'company-backends)
+				    'company-org-block))))
 
 ;;; init-40-org.el ends here
