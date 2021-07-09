4fbd539b002734aa3509ce07b7e81dbc1eeade1e
Thomas Jost <schnouki@schnouki.net>
2021-07-09 09:10:04 +0200

    emacs: fix fixed-pitch face

--- emacs/init-10-fonts.el
+++ emacs/init-10-fonts.el
@@ -8,6 +8,6 @@
   (add-to-list 'default-frame-alist
 	       `(font . ,default-font))
   (set-frame-font default-font)
-  (set-face-attribute 'fixed-pitch nil :family default-font))
+  (set-face-attribute 'fixed-pitch nil :font default-font))
 
 ;;; init-10-fonts.el ends here
