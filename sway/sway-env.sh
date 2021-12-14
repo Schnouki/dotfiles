a7d9018e7c8e442d08cc8c9c867fb856630b1792
Thomas Jost <schnouki@schnouki.net>
2021-12-14 16:27:32 +0100

    sway: don't force wayland for SDL
    It doesn't work well enough.
    
    Also, apprently setting GDK_BACKEND=wayland is a bad idea? Let's try.

--- sway/sway-env.sh
+++ sway/sway-env.sh
@@ -2,7 +2,7 @@
 export XDG_CURRENT_DESKTOP=sway
 
 # GTK
-export GDK_BACKEND=wayland
+# export GDK_BACKEND=wayland
 export GTK2_RC_FILES="$HOME/.config/gtkrc-2.0:$HOME/.config/gtkrc-$(hostname):/etc/gtk-2.0/gtkrc"
 
 # Qt
@@ -13,7 +13,7 @@ export QT_STYLE_OVERRIDE=gtk
 export CLUTTER_BACKEND=wayland
 
 # SDL2
-export SDL_VIDEODRIVER=wayland
+# export SDL_VIDEODRIVER=wayland
 
 # Firefox & Thunderbird
 export MOZ_ENABLE_WAYLAND=1
