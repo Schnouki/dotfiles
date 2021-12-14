653988339a26c8836f605533ece7c72ef59b8791
Thomas Jost <schnouki@schnouki.net>
2021-12-14 16:29:51 +0100

    fish: update vf and python

--- fish/conf.d/virtualfish-loader.fish
+++ fish/conf.d/virtualfish-loader.fish
@@ -1,9 +1,9 @@
-set -g VIRTUALFISH_VERSION 2.5.1
+set -g VIRTUALFISH_VERSION 2.5.4
 set -g VIRTUALFISH_PYTHON_EXEC /usr/bin/python
 set -g VIRTUALFISH_ACTIVATION_FILE .workon
 
-source /usr/lib/python3.9/site-packages/virtualfish/virtual.fish
-source /usr/lib/python3.9/site-packages/virtualfish/environment.fish
-source /usr/lib/python3.9/site-packages/virtualfish/compat_aliases.fish
-source /usr/lib/python3.9/site-packages/virtualfish/auto_activation.fish
+source /usr/lib/python3.10/site-packages/virtualfish/virtual.fish
+source /usr/lib/python3.10/site-packages/virtualfish/environment.fish
+source /usr/lib/python3.10/site-packages/virtualfish/compat_aliases.fish
+source /usr/lib/python3.10/site-packages/virtualfish/auto_activation.fish
 emit virtualfish_did_setup_plugins
