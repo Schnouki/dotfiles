112fb52874e0e1496470a06f0e659876cdd2d5bb
Thomas Jost <schnouki@schnouki.net>
2021-12-30 16:32:52 +0100

    emacs: improve font setup for Emacs 29

--- emacs/init-91-fonts.el
+++ emacs/init-91-fonts.el
@@ -2,12 +2,34 @@
 ;;; Commentary:
 ;;; Code:
 
-;; Default font
-(let ((default-font "Iosevka-12"))
-  (assq-delete-all 'font default-frame-alist)
-  (add-to-list 'default-frame-alist
-	       `(font . ,default-font))
-  (set-frame-font default-font)
-  (set-face-attribute 'fixed-pitch nil :font default-font))
+(require 'dash)
+
+(defun schnouki/setup-fonts ()
+  "Setup fonts."
+  (interactive)
+  (let* ((mono-family "Iosevka")
+	 (mono-serif-family "Iosevka Slab")
+	 (var-family "Iosevka Aile")
+	 (font-size 12)
+	 (mono-font (format "%s-%d" mono-family font-size))
+	 (mono-serif-font (format "%s-%d" mono-serif-family font-size))
+	 (var-font (format "%s-%d" var-family font-size)))
+
+    (set-face-attribute 'default nil :font mono-font)
+    (set-face-attribute 'fixed-pitch nil :font mono-font)
+    (set-face-attribute 'fixed-pitch-serif nil :font mono-serif-font)
+    (set-face-attribute 'variable-pitch nil :font var-font)
+
+    (assq-delete-all 'font default-frame-alist)
+    (add-to-list 'default-frame-alist
+		 `(font . ,mono-font))
+    (set-frame-font mono-font)
+
+    (--each '(mode-line mode-line-active mode-line-inactive)
+      (set-face-attribute it nil :font mono-font))))
+
+(eval-after-load 'faces
+  (schnouki/setup-fonts))
+
 
 ;;; init-91-fonts.el ends here
