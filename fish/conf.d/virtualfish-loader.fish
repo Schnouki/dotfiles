7176fbd0433db0596e3f3cd5ae69ce6d14f7fc7c
Thomas Jost <schnouki@schnouki.net>
2022-03-22 11:28:17 +0100

    fish: improve virtualfish setup

--- fish/conf.d/virtualfish-loader.fish
+++ fish/conf.d/virtualfish-loader.fish
@@ -2,8 +2,15 @@ set -g VIRTUALFISH_VERSION 2.5.4
 set -g VIRTUALFISH_PYTHON_EXEC /usr/bin/python
 set -g VIRTUALFISH_ACTIVATION_FILE .workon
 
-source /usr/lib/python3.10/site-packages/virtualfish/virtual.fish
-source /usr/lib/python3.10/site-packages/virtualfish/environment.fish
-source /usr/lib/python3.10/site-packages/virtualfish/compat_aliases.fish
-source /usr/lib/python3.10/site-packages/virtualfish/auto_activation.fish
+set -l _vf_py_ver ($VIRTUALFISH_PYTHON_EXEC -c 'import sys; print(f"{sys.version_info.major}.{sys.version_info.minor}")')
+set -l _vf_path "/usr/lib/python$_vf_py_ver/site-packages/virtualfish"
+
+if test -e "$HOME/.local/lib/python$_vf_py_ver/site-packages/virtualfish/virtual.fish"
+    set _vf_path "$HOME/.local/lib/python$_vf_py_ver/site-packages/virtualfish"
+end
+
+source "$_vf_path/virtual.fish"
+source "$_vf_path/environment.fish"
+source "$_vf_path/compat_aliases.fish"
+source "$_vf_path/auto_activation.fish"
 emit virtualfish_did_setup_plugins
