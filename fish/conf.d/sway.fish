e32c3afefa9081ef235d7f699b64c0fed809bc40
Thomas Jost <schnouki@schnouki.net>
2023-06-14 15:15:26 +0200

    fish: add sway/wayland environment variables

---
+++ fish/conf.d/sway.fish
@@ -0,0 +1,19 @@
+# Generic
+set -Ux XDG_CURRENT_DESKTOP sway
+
+# GTK
+# set -Ux GDK_BACKEND wayland
+set -Ux GTK2_RC_FILES "$HOME/.config/gtkrc-2.0:$HOME/.config/gtkrc-(hostnamectl hostname):/etc/gtk-2.0/gtkrc"
+
+# Qt
+set -Ux QT_QPA_PLATFORM wayland
+set -Ux QT_STYLE_OVERRIDE gtk
+
+# Clutter
+set -Ux CLUTTER_BACKEND wayland
+
+# SDL2
+# set -Ux SDL_VIDEODRIVER wayland
+
+# Firefox & Thunderbird
+set -Ux MOZ_ENABLE_WAYLAND 1
