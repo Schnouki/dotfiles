6517336c3e53ed69d74f54c1c8ccc78d05c04934
Thomas Jost <schnouki@schnouki.net>
2023-09-12 01:02:52 +0200

    rm spotifyd

--- spotifyd/run-spotifyd.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-exec env \
-     PULSE_PROP_application.name=spotifyd \
-     PULSE_PROP_application.icon_name=spotify \
-     PULSE_PROP_media.role=music \
-     /usr/bin/spotifyd "$@"
