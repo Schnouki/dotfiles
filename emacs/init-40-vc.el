b162f413d57f6e4687250eb595da871f4376aa41
Thomas Jost <schnouki@schnouki.net>
2023-09-28 14:43:45 +0200

    emacs: attempt to disable pinentry in emacs

--- emacs/init-40-vc.el
+++ emacs/init-40-vc.el
@@ -23,7 +23,7 @@
   :custom
   (magit-process-popup-time 2)
   (vc-follow-symlinks t)
-  (epg-pinentry-mode 'loopback)
+  (epg-pinentry-mode nil)
   :config
 
   (setq schnouki/magit-process-buffer-tail t)
