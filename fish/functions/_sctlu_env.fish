395f0015bbc51d9fb65d61687f03452fb4a303ec
Thomas Jost <schnouki@schnouki.net>
2023-10-26 12:03:43 +0200

    fish/systemd: import Sway environment variable in systemd

--- fish/functions/_sctlu_env.fish
+++ fish/functions/_sctlu_env.fish
@@ -2,8 +2,9 @@ function _sctlu_env -d "Import relevant environment variables in systemctl --use
     # Variables known to be imported
     set -l vars PATH XDG_DATA_HOME XDG_STATE_HOME XDG_CONFIG_HOME XDG_CACHE_HOME
 
-    # Read more variables from xdg.fish
+    # Read more variables from xdg.fish and sway.fish
     set --append vars (awk '/^set -Ux/ {print $3}' ~/.config/fish/conf.d/xdg.fish)
+    set --append vars (awk '/^set -Ux/ {print $3}' ~/.config/fish/conf.d/sway.fish)
 
     systemctl --user import-environment $vars
 end
