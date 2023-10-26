a71fcc578be0682d0fd24f618d3ad64aafb313c4
Thomas Jost <schnouki@schnouki.net>
2023-10-26 12:01:05 +0200

    emacs: improve modeline

--- emacs/init-95-modeline.el
+++ emacs/init-95-modeline.el
@@ -5,26 +5,24 @@
 ;; Smart mode line
 (use-package smart-mode-line
   :ensure t
-  :config
-  (setq sml/theme 'respectful
-        sml/show-client t
-        rm-blacklist '(" ,"           ; subword
-                       " Abbrev"      ; abbrev
-                       ;;" AC"          ; auto-complete
-                       " ElDoc"       ; eldoc
-                       )
-        sml/replacer-regexp-list
-        '(("^/ssh:.*:"             ":SSH:")
-          ("^/sudo:.*:"            ":SU:")
-          ("^~/.config/"           ":C:")
-          ("^~/Dropbox/"           ":DB:")
-          ("^~/stibidik/"          ":S:")
-          ("^:S:web/src/stibidik/" ":Sw:")
-          ("^~/doist/"             ":D:")
-          ("^:D:Todoist/"          ":TD:")
-          ("^:D:twist/"            ":TW:")
-          ))
+  :custom
+  (sml/theme 'respectful)
+  (sml/show-client t)
+  (rm-blacklist '(" ,"           ; subword
+                  " Abbrev"      ; abbrev
+                  " ElDoc"))      ; eldoc
+  (sml/replacer-regexp-list
+   '(("^/ssh:.*:"    ":SSH:")
+     ("^/sudo:.*:"   ":SU:")
+     ("^~/.config/"  ":C:")
+     ("^~/Dropbox/"  ":DB:")
+     ("^~/doist/"    ":D:")
+     ("^:D:Todoist/" ":TD:")
+     ("^:D:twist/"   ":TW:")))
+  (sml/name-width '(0 . 44))
+  (sml/mode-width 'full)
 
+  :config
   ;; Move which-func indicator to just after the file name
   ;; https://github.com/Bruce-Connor/smart-mode-line/issues/77#issuecomment-39708760
   (let ((which-func '(which-func-mode ("" which-func-format " ")))
