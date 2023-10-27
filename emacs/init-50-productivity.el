cb864bdb175ed48782efaa5033efc402ae5cd2ee
Thomas Jost <schnouki@schnouki.net>
2023-10-28 01:17:15 +0200

    emacs: add pomm
    Yay for third-time!

---
+++ emacs/init-50-productivity.el
@@ -0,0 +1,14 @@
+;;; 50-productivity --- Productivity stuff
+;;; Commentary:
+;;; Code:
+
+(use-package pomm
+  :ensure t
+  :bind (:map schnouki-prefix-map
+              ("t" . pomm-third-time))
+  :custom
+  (pomm-third-time-fraction "1/4")
+  :init
+  (pomm-mode-line-mode 1))
+
+;;; init-50-productivity.el ends here
