e5273d92f0c5a0850b5b68f13498e70d0c00d72e
Thomas Jost <schnouki@schnouki.net>
2023-10-30 08:05:51 +0100

    emacs: focus-in-hook -> after-focus-change-function

--- emacs/init-40-python.el
+++ emacs/init-40-python.el
@@ -6,9 +6,13 @@
 (use-package auto-virtualenvwrapper
   :ensure t
   :commands auto-virtualenvwrapper-activate
-  :hook ((python-mode window-configuration-change focus-in) . auto-virtualenvwrapper-activate)
-  :custom
-  (auto-virtualenvwrapper-verbose nil))
+  :hook ((python-mode window-configuration-change) . auto-virtualenvwrapper-activate)
+  :init
+  (setq auto-virtualenvwrapper-verbose nil)
+  (defun schnouki/ava-on-focus-change ()
+    (when (frame-focus-state)
+      (auto-virtualenvwrapper-activate)))
+  (add-function :before after-focus-change-function #'schnouki/ava-on-focus-change))
 
 
 (use-package cython-mode
