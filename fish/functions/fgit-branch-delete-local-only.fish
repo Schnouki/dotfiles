54a58c17f275f24070c90124c0b2b3c8a592be40
Thomas Jost <schnouki@schnouki.net>
2023-12-07 12:59:11 +0100

    fish: improve git-fzf functions

--- fish/functions/fgit-branch-delete-local-only.fish
+++ fish/functions/fgit-branch-delete-local-only.fish
@@ -1,6 +1,6 @@
 function fgit-branch-delete-local-only -d "Delete branches that are not tracked on any remote"
     LC_ALL=C git branch --format '%(refname:short) REMOTE:%(upstream:short)%(upstream:track)' \
-        | egrep 'REMOTE:(\[gone\])?$' \
+        | egrep 'REMOTE:((.*\[gone\]))?$' \
         | cut -d" " -f1 \
         | fzf --multi --preview="git lg {}" --preview-window=up:75% --prompt="Delete branches> " --layout=reverse \
         | xargs --no-run-if-empty git branch --delete --force
