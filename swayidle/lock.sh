16d7441e28dd81125ccc51165ee89faa541d801b
Thomas Jost <schnouki@schnouki.net>
2024-01-17 12:16:24 +0100

    swayidle: enable swaylock grace period on idle only, not on suspend

--- swayidle/lock.sh
+++ swayidle/lock.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
+reason=$1
+swaylock_args=()
+
+if [[ "$reason" == "idle" ]]; then
+    swaylock_args+=("--grace" 5)
+fi
+
 logger -t lock.sh "Syncing before lock..."
 sync
 
 logger -t lock.sh "Locking!"
-exec swaylock -f
+exec swaylock -f "${swaylock_args[@]}"
