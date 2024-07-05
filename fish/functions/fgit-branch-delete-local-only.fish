f15e3957f90e470d597627057e675be859307d96
Thomas Jost <schnouki@schnouki.net>
2024-07-05 11:36:42 +0200

    fish: improve fgit-branch-delete-local-only

--- fish/functions/fgit-branch-delete-local-only.fish
+++ fish/functions/fgit-branch-delete-local-only.fish
@@ -1,6 +1,13 @@
 function fgit-branch-delete-local-only -d "Delete branches that are not tracked on any remote"
+    # Regexp for "grep -P": match the local branch name, then after REMOTE: it
+    # must either be:
+    #   - [gone]
+    #   - end-of-line ($)
+    #   - a remote branch name (\S+/) that doesn't match the local one (negative
+    #     lookahead on \1).
+
     LC_ALL=C git branch --format '%(refname:short) REMOTE:%(upstream:short)%(upstream:track)' \
-        | egrep 'REMOTE:((.*\[gone\]))?$' \
+        | grep -P '^(\S+) REMOTE:(\[gone\]|$|\S+/(?!\1))' \
         | cut -d" " -f1 \
         | fzf --multi --preview="git lg {}" --preview-window=up:75% --prompt="Delete branches> " --layout=reverse \
         | xargs --no-run-if-empty git branch --delete --force
