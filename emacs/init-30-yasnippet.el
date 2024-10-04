2b56f5f0b9bbd23ba3fb2445e8c93e77d3ed7780
Thomas Jost <schnouki@schnouki.net>
2024-10-04 17:18:56 +0200

    emacs: remove now useless yasnippet tweaks

--- emacs/init-30-yasnippet.el
+++ emacs/init-30-yasnippet.el
@@ -18,14 +18,4 @@
 (use-package yasnippet-snippets
   :ensure t)
 
-;; Modes that bind something to "<tab>" instead of "TAB"
-;; -- http://blog.iany.me/2012/03/fix-tab-binding-for-yasnippet-and-auto-complete/
-(defun schnouki/yas-tab-noconflict ()
-  (interactive)
-  (let ((command (key-binding [tab])))    ; remember command
-    (local-unset-key [tab])               ; unset from (kbd "<tab>")
-    (local-set-key (kbd "TAB") command))) ; bind to (kbd "TAB")
-(add-hook 'ruby-mode-hook 'schnouki/yas-tab-noconflict)
-(add-hook 'markdown-mode-hook 'schnouki/yas-tab-noconflict)
-
 ;;; init-30-yasnippet.el ends here
