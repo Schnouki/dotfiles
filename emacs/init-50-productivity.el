09b1486e01ba8b6552da0023a7f56acf71966cf8
Thomas Jost <schnouki@schnouki.net>
2025-02-25 11:57:35 +0100

    emacs: expose pomm mode-line over D-Bus

--- emacs/init-50-productivity.el
+++ emacs/init-50-productivity.el
@@ -9,6 +9,11 @@
   :custom
   (pomm-third-time-fraction "1/4")
   :config
-  (pomm-mode-line-mode 1))
+  (pomm-mode-line-mode 1)
+
+  ;; Expose modeline over D-Bus
+  (defun schnouki/update-pomm-mode-line-dbus-property (&rest r)
+    (dbus-register-property :session dbus-service-emacs "/net/schnouki/pomm" "net.schnouki.pomm" "mode-line" :read pomm-current-mode-line-string t))
+  (advice-add #'pomm-third-time-update-mode-string :after #'schnouki/update-pomm-mode-line-dbus-property))
 
 ;;; init-50-productivity.el ends here
