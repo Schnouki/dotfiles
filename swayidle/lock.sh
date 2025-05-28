b41a3f37873c3cf874eb2752a27d11474a662666
Thomas Jost <schnouki@schnouki.net>
2025-05-28 03:26:53 +0200

    swayidle: explain how it works

--- swayidle/lock.sh
+++ swayidle/lock.sh
@@ -3,6 +3,8 @@
 reason=$1
 swaylock_args=()
 
+# If locking because idle, give a grace period of 5 seconds before
+# requiring a password
 if [[ "$reason" == "idle" ]]; then
     swaylock_args+=("--grace" 5)
 fi
