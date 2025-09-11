7acf82218801b317922ce919b898d75b8d93699d
Thomas Jost <schnouki@schnouki.net>
2025-09-11 18:24:33 +0200

    emacs: tweak TRAMP performance

--- emacs/init-40-tramp.el
+++ emacs/init-40-tramp.el
@@ -2,13 +2,34 @@
 ;;; Commentary:
 ;;; Code:
 
-;; Share connections with SSH
-;; https://elpa.gnu.org/packages/doc/tramp.html#Ssh-setup
-(with-eval-after-load 'tramp-sh
-  (setq tramp-ssh-controlmaster-options
-        (concat "-o ControlMaster=auto "
-                "-o ControlPersist=yes "
-                "-o ControlPath=/tmp/ssh-schnouki-%%r@%%h:%%p")))
+;; Documentation: https://elpa.gnu.org/packages/doc/tramp.html
 
+;; Make TRAMP a bit faster
+;; https://www.gnu.org/software/tramp/#Frequently-Asked-Questions-1
+(setopt vc-ignore-dir-regexp (format "\\(%s\\)\\|\\(%s\\)"
+                                     vc-ignore-dir-regexp
+                                     tramp-file-name-regexp)
+        shell-history-file-name t
+        tramp-verbose 2)
+
+;; Making TRAMP go brrrr...
+;; https://coredumped.dev/2025/06/18/making-tramp-go-brrrr./
+(setopt remote-file-name-inhibit-locks t
+        tramp-use-scp-direct-remote-copying t
+        remote-file-name-inhibit-auto-save-visited t
+        tramp-copy-size-limit (* 1 1024 1024) ;; 1 MB
+        magit-tramp-pipe-stty-settings 'pty)
+
+(connection-local-set-profile-variables
+ 'remote-direct-async-process
+ '((tramp-direct-async-process . t)))
+
+(connection-local-set-profiles
+ '(:application tramp :protocol "scp")
+ 'remote-direct-async-process)
+
+(with-eval-after-load 'tramp
+  (with-eval-after-load 'compile
+    (remove-hook 'compilation-mode-hook #'tramp-compile-disable-ssh-controlmaster-options)))
 
 ;;; init-40-tramp.el ends here
