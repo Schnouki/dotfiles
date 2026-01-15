b90eee48c73596d4d9137b805cc11e20e7b11b6d
Thomas Jost <schnouki@schnouki.net>
2026-01-15 11:15:50 +0100

    fish: update pueue completion

--- fish/completions/pueue.fish
+++ fish/completions/pueue.fish
@@ -1,2 +1,2 @@
-source /usr/share/fish/completions/pueue.fish
+source /usr/share/fish/vendor_completions.d/pueue.fish
 complete -c pueue -n "__fish_seen_subcommand_from add" -d 'The command to be added' -x -a '(__fish_complete_subcommand --fcs-skip=2)'
